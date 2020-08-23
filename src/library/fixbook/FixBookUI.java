e6a49a0c7331c667217753dd064d05e367f77e38
Akshay Patel <62536177+carnival067@users.noreply.github.com>
2020-08-24 00:48:54 +1000

    update seState method
     changed method name SeT_StAtE to setState and uI_sTaTe to UIState and changed StAtE to state

--- src/library/fixbook/FixBookUI.java
+++ src/library/fixbook/FixBookUI.java
@@ -31,8 +31,10 @@ public class FixBookUI {
 	}
 
 
-	public void SeT_StAtE(uI_sTaTe state) {
-		this.StAtE = state;
+	// public void SeT_StAtE(uI_sTaTe state) {
+	public void setState(UIState state) {						// changed method name SeT_StAtE to setState and uI_sTaTe to UIState
+		// this.StAtE = state;
+		this.state = state;							// changed StAtE to state
 	}
 
 	
